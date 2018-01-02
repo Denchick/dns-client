09e783aa9b9f12864b268d74b0a9dfb53540ec18
Volkov Denis <denchick1997@mail.com>
2018-01-03 03:25:44 +0500

    add tests

--- tests/test_request.py
+++ tests/test_request.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              os.path.pardir))
 
-from architecture.request import RequestHeader
+from architecture.request import RequestHeader, Request
 
 
 class TestRequest(unittest.TestCase):
@@ -13,6 +13,22 @@ class TestRequest(unittest.TestCase):
     def setUp(self):
         self.header_with_rd = RequestHeader(recursion_desired=True)
         self.header_without_rd = RequestHeader(recursion_desired=False)
+
+    def test_RequestInit_shouldSetTypeRecordToOne_whenTypeRecordIsNone(self):
+        request = Request('yandex.ru', True, None)
+        self.assertEqual(request.type_record, 1)
+
+    def test_RequestInit_shouldRaiseKeyError_whenTypeRecordIsUnknown(self):
+        with self.assertRaises(AttributeError):
+            Request('yandex.ru', True, 228)
+
+    def test_RequestInit_shouldCorrectlySetFields(self):
+        request = Request('yandex.ru', True, 'A')
+        self.assertEqual(request.domain_name, 'yandex.ru')
+        self.assertTrue(request.recursion_desired)
+        self.assertEqual(request.request_class, 1)
+        expected = b'\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x06yandex\x02ru\x00\x00\x01\x00\x01'
+        self.assertEqual(request._message[2:], expected)
 
     def test_correctEncodingHeader_whenRecursionNotDesired(self):
         actual = self.header_without_rd._encode()
