d58d3a2cab22e14752b966e822a9a586ccb68309
Volkov Denis <denchick1997@mail.com>
2018-01-03 03:07:07 +0500

    add tests

--- tests/test_response.py
+++ tests/test_response.py
@@ -5,7 +5,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              os.path.pardir))
 
-from architecture.response import ResponseQuery, ResponseRecordType
+from architecture.response import ResponseQuery, ResponseRecordType, Response
 from architecture.response import ResponseHeader
 
 
@@ -16,11 +16,12 @@ class TestResponse(unittest.TestCase):
         self.raw_message2 = b'\xbd\x17\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x04mail\x06yandex\x02ru\x00\x00\x1c\x00\x01\xc0\x0c\x00\x1c\x00\x01\x00\x00\x00\xd8\x00\x10*\x02\x06\xb8\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00%'
 
     def test_response_init(self):
-        query = ResponseQuery(self.raw_message, 12)
-        self.assertEqual(query._current_offset, 32)
-        self.assertEqual(query._domain_name, 'mail.yandex.ru')
-        self.assertEqual(query._type, 1)
-        self.assertEqual(query._request_class, 1)
+        response = Response(self.raw_message)
+        self.assertEqual(response.header, ResponseHeader(self.raw_message))
+        self.assertEqual(len(response.questions), 1)
+        self.assertEqual(len(response.answers), 5)
+        self.assertEqual(response.authority_RRs, [])
+        self.assertEqual(response.additional_RRs, [])
 
     def test_responseHeader(self):
         header = ResponseHeader(self.raw_message)
@@ -48,7 +49,8 @@ class TestResponse(unittest.TestCase):
         self.assertEqual(record.request_class, 1)
         self.assertEqual(record.ttl, 216)
         self.assertEqual(record.get_offset(), 60)
-        self.assertEqual(str(record), str(record))
+        expected = '    Name: mail.yandex.ru\n    Type: AAAA\n    Class: IN\n    TTL: 216\n2a02:6b8::::::25\n'
+        self.assertEqual(str(record), expected)
 
 if __name__ == "__main__":
     unittest.main()
